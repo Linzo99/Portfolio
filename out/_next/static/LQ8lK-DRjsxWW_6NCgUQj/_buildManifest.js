b2860051d23c5dc18a13a3ed49b7eb81ff34aeba
Linzo99 <sallalioune99@gmail.com>
2022-02-19 23:19:28 +0000

    generated static files

---
+++ out/_next/static/LQ8lK-DRjsxWW_6NCgUQj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/333-fb8feb74e53922eb.js","static/chunks/pages/index-f597646982bbd51d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
